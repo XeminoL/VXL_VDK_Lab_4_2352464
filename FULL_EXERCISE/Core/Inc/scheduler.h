31e7f35777b07acd4410b04b878877c312abe759
Khang <dangthaikhang34@gmail.com>
2025-10-26 23:08:51 +0700

    Add files via upload

---
+++ FULL_EXERCISE/Core/Inc/scheduler.h
@@ -0,0 +1,22 @@
+#ifndef INC_SCHEDULER_H_
+#define INC_SCHEDULER_H_
+
+#include "stm32f1xx_hal.h"
+
+#define SCH_MAX_TASKS 10
+
+typedef struct {
+  void (*pTask)(void);  
+  uint32_t Delay;    
+  uint32_t Period;     
+  uint8_t RunMe;        
+  uint32_t TaskID;      
+} sTask;
+
+void SCH_Init(void);
+void SCH_Update(void);
+void SCH_Dispatch_Tasks(void);
+uint32_t SCH_Add_Task(void (*pFunction)(), uint32_t DELAY, uint32_t PERIOD);
+uint8_t SCH_Delete_Task(uint32_t taskID);
+
+#endif 
